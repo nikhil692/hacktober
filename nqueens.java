232fe3e8f362857c3f0e872327f3cd9344d579fd
MK Nikhil <mknikhil692@gmail.com>
2022-10-02 01:26:22 +0530

    problem

---
+++ nqueens.java
@@ -0,0 +1,13 @@
+public class NQueenProblem {
+    final int N = 4;
+  
+     A utility function to print solution 
+    void printSolution(int board[][])
+    {
+        for (int i = 0; i  N; i++) {
+            for (int j = 0; j  N; j++)
+                System.out.print(  + board[i][j]
+                                 +  );
+            System.out.println();
+        }
+    }
